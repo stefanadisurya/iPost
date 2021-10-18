99bd999d59cbb9abc483a5fdef18cb30dfd723da
Stefan Adisurya <stefanadisurya@gmail.com>
2021-10-18 12:51:09 +0700

    modify UserDetail page

--- iPost/Feature/Pages/UserDetail/Controller/UserDetailViewController.swift
+++ iPost/Feature/Pages/UserDetail/Controller/UserDetailViewController.swift
@@ -11,8 +11,46 @@ class UserDetailViewController: UIViewController {
     
     var userId: Int?
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var companyLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        getUserById(userId ?? 1)
+    }
+    
+    private func getUserById(_ id: Int) {
+        let request = NSMutableURLRequest(url: NSURL(string: "https://jsonplaceholder.typicode.com/users/\(id)")! as URL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
+        
+        request.httpMethod = "GET"
+        
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            if error != nil {
+                print(error as Any)
+            } else {
+                if let data = data {
+                    do {
+                        let dataJSON = try JSONDecoder().decode(User.self, from: data)
+                        
+                        DispatchQueue.main.async {
+                            self.nameLabel.text = dataJSON.name
+                            self.emailLabel.text = dataJSON.email
+                            self.addressLabel.text = "\(dataJSON.address.street), \(dataJSON.address.suite), \(dataJSON.address.city)"
+                            self.companyLabel.text = "\(dataJSON.company.name)"
+                        }
+                    } catch let error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        })
+        
+        dataTask.resume()
     }
     
 }
