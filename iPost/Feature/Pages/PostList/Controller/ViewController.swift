20f7b4e9daffdc5cca1f63e4688178c72329e14a
Stefan Adisurya <stefanadisurya@gmail.com>
2021-10-21 15:35:48 +0700

    add generic function

--- iPost/Feature/Pages/PostList/Controller/ViewController.swift
+++ iPost/Feature/Pages/PostList/Controller/ViewController.swift
@@ -11,53 +11,49 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var dataArray: [Post] = []
-    var userArray: [User] = []
+    var posts: [Post] = []
+    var users: [User] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setUpCell()
-        loadDataFromAPI {
-            for dt in self.dataArray {
+        
+        getAllPost {
+            for dt in self.posts {
                 self.getUserById(id: dt.userId)
             }
-
-            self.tableView.reloadData()
         }
     }
     
     private func setUpCell() {
         tableView.register(UINib(nibName: "PostTableViewCell", bundle: nil), forCellReuseIdentifier: "PostCell")
     }
-
-    private func loadDataFromAPI(completion: @escaping () -> ()) {
-        ConsumeAPI.loadData(from: "https://jsonplaceholder.typicode.com/posts") { data, response, error in
-            guard let data = data, error == nil else { return }
-            
-            do {
-                self.dataArray = try JSONDecoder().decode([Post].self, from: data)
-
+    
+    private func getAllPost(completion: @escaping () -> ()) {
+        URLSession.shared.request(url: Constants.postUrl, expecting: [Post].self) { [weak self] result in
+            switch result {
+            case .success(let posts):
                 DispatchQueue.main.async {
+                    self?.posts = posts
+                    self?.tableView.reloadData()
                     completion()
                 }
-            } catch let error {
+            case .failure(let error):
                 print(error.localizedDescription)
             }
         }
     }
     
     private func getUserById(id: Int) {
-        ConsumeAPI.loadData(from: "https://jsonplaceholder.typicode.com/users/\(id)") { data, response, error in
-            guard let data = data, error == nil else { return }
-            
-            do {
-                self.userArray.append(try JSONDecoder().decode(User.self, from: data))
-                
+        URLSession.shared.request(url: URL(string: "https://jsonplaceholder.typicode.com/users/\(id)"), expecting: User.self) { [weak self] result in
+            switch result {
+            case .success(let user):
                 DispatchQueue.main.async {
-                    self.tableView.reloadData()
+                    self?.users.append(user)
+                    self?.tableView.reloadData()
                 }
-            } catch let error {
+            case .failure(let error):
                 print(error.localizedDescription)
             }
         }
@@ -68,19 +64,22 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataArray.count
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostTableViewCell
-        cell.postTitle.text = dataArray[indexPath.row].title.capitalized
-        cell.postBody.text = dataArray[indexPath.row].body.capitalized
+        cell.postTitle.text = posts[indexPath.row].title.capitalized
+        cell.postBody.text = posts[indexPath.row].body.capitalized
         
-        ConsumeAPI.loadData(from: "https://jsonplaceholder.typicode.com/users?userId=\(self.dataArray[indexPath.row].userId)") {
-            data, response, error in
-            guard let _ = data, error == nil else { return }
-            DispatchQueue.main.async() {
-                cell.userNameAndCompany.text = "\(self.userArray[indexPath.row].name) | \(self.userArray[indexPath.row].company.name)"
+        URLSession.shared.get(url: URL(string: "https://jsonplaceholder.typicode.com/users?userId=\(self.posts[indexPath.row].userId)")) { result in
+            switch result {
+            case .success():
+                DispatchQueue.main.async {
+                    cell.userNameAndCompany.text = "\(self.users[indexPath.row].name) | \(self.users[indexPath.row].company.name)"
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
             }
         }
         
@@ -90,11 +89,11 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "PostDetail", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "PostDetail") as! PostDetailViewController
-        vc.userId = dataArray[indexPath.row].userId
-        vc.postId = dataArray[indexPath.row].id
-        vc.postTitle = dataArray[indexPath.row].title
-        vc.postBody = dataArray[indexPath.row].body
-        vc.username = userArray[indexPath.row].name
+        vc.userId = posts[indexPath.row].userId
+        vc.postId = posts[indexPath.row].id
+        vc.postTitle = posts[indexPath.row].title
+        vc.postBody = posts[indexPath.row].body
+        vc.username = users[indexPath.row].name
         
         navigationController?.pushViewController(vc, animated: true)
     }
--- iPost/Utilities/Network/ConsumeAPI.swift
+++ iPost/Utilities/Network/ConsumeAPI.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+struct Constants {
+    static let userUrl = URL(string: "https://jsonplaceholder.typicode.com/users/")
+    static let postUrl = URL(string: "https://jsonplaceholder.typicode.com/posts")
+}
+
 class ConsumeAPI {
     
     static let session = URLSession.shared
@@ -25,6 +30,68 @@ class ConsumeAPI {
         })
         
         dataTask.resume()
+    }
+    
+}
+
+extension URLSession {
+    
+    enum CustomError: Error {
+        case invalidUrl
+        case invalidData
+    }
+    
+    func request<T: Codable>(url: URL?, expecting: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
+        guard let url = url else {
+            completion(.failure(CustomError.invalidUrl))
+            return
+        }
+        
+        let task = self.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.failure(CustomError.invalidData))
+                }
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(expecting, from: data)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func get(url: URL?, completion: @escaping (Result<(), Error>) -> Void) {
+        guard let url = url else {
+            completion(.failure(CustomError.invalidUrl))
+            return
+        }
+        
+        let task = self.dataTask(with: url) { data, _, error in
+            guard let data = data else {
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.failure(CustomError.invalidData))
+                }
+                return
+            }
+            
+            do {
+                completion(.success(()))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
     }
     
 }
